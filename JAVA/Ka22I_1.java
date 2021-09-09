d76ee4301091b688ae36eb2087457efd5d399838
sungjin <sungjin0757@naver.com>
2021-09-10 01:07:12 +0900

    프로그래머스 풀이

---
+++ JAVA/Ka22I_1.java
@@ -0,0 +1,19 @@
+package kakao;
+
+class Ka22I_1 {
+    public int solution(String s) {
+        int answer = 0;
+        s=s.replace("zero","0");
+        s=s.replace("one","1");
+        s=s.replace("two","2");
+        s=s.replace("three","3");
+        s=s.replace("four","4");
+        s=s.replace("five","5");
+        s=s.replace("six","6");
+        s=s.replace("seven","7");
+        s=s.replace("eight","8");
+        s=s.replace("nine","9");
+        answer=Integer.parseInt(s);
+        return answer;
+    }
+}
